a97703ac45a7281eb5732316b1949a66f3f42ce6
ponCHANNN <mizuki.7302@gmail.com>
2022-11-26 13:39:45 +0900

    add nagari to jp

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const trans = require("./jp_to_nagari.json");
-second({argv: process.argv[2]})
+const transJpNagari = require("./jp_to_nagari.json");
+const transNagariJp = require("./nagari_to_jp.json");
+const fs = require('fs');
+let cont = fs.readFileSync("content.txt", 'utf-8');
+jpToNagari({argv: process.argv[2]});
+//nagariToJp({argv: process.argv[2]});
 
-function main(options) {
+function jpToNagari(options) {
     let content = options.argv;
+    if (content == undefined) content = cont;
     for (let i = 0; i < content.length; i++) {
-        console.log(trans[content[i]]);
+        process.stdout.write(transJpNagari[content[i]] !== undefined? transJpNagari[content[i]] : content[i]);
     }
 }
 
 
-const flip = (data) => Object.fromEntries(
-    Object
-      .entries(data)
-      .map(([key, value]) => [value, key])
-    );
-
-function second(options) {
+function nagariToJp(options) {
     let content = options.argv;
-    for (let i = 0; content.length; i++) {
-        console.log(flip(trans)[content[i]]);
+    if (content == undefined) content = cont;
+    for (let i = 0; i < content.length; i++) {
+        process.stdout.write(transNagariJp[content[i]] !== undefined? transNagariJp[content[i]] : content[i]);
     }
 }
